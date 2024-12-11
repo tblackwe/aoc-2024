6331326b7945e9aed6b4197b1a285cc92e041a9d
Thomas Blackwell <thomas.blackwell@coforma.io>
2024-12-11 11:19:22 -0500

    order isnt important. got the clue from a reddit meme :shame:

--- src/11.py
+++ src/11.py
@@ -1,4 +1,5 @@
 from utils.api import get_input, load_sample
+from collections import defaultdict
 
 input_str = get_input(11)
 test_data = load_sample(11)
@@ -8,44 +9,71 @@ test_data = load_sample(11)
 # Part 1
 
 def rule_1(stone):
-    return '1' if stone == '0' else stone
+    return 1 if stone == 0 else stone
 def rule_2(stone):
-    if len(stone) % 2 == 0:
-        mid_index = len(stone) // 2
-        first_stone = str(int(stone[:mid_index]))
-        second_stone = str(int(stone[mid_index:]))
+    str_stone = str(stone)
+    if len(str_stone) % 2 == 0:
+        mid_index = len(str_stone) // 2
+        first_stone = int(str_stone[:mid_index])
+        second_stone = int(str_stone[mid_index:])
         return [first_stone, second_stone]
     else:
         return stone
 def rule_3(stone):
-    return str(int(stone) * 2024)
+    return int(stone) * 2024
 
-def blink(stones):
-    updated_stones = []
-    for stone in stones:
-        updated_stone = rule_1(stone)
-        if updated_stone != stone:
-            updated_stones.append(updated_stone)
-        else:
-            updated_stone = rule_2(stone)
+
+
+def solution_1(input):
+    def blink(stones):
+        updated_stones = []
+        for stone in stones:
+            updated_stone = rule_1(stone)
             if updated_stone != stone:
-                updated_stones+= updated_stone
-                # updated_stones.append(updated_stone[1])
-
+                updated_stones.append(updated_stone)
             else:
-                updated_stones.append(rule_3(stone))
-    return updated_stones
+                updated_stone = rule_2(stone)
+                if updated_stone != stone:
+                    updated_stones+= updated_stone
+                    # updated_stones.append(updated_stone[1])
 
-def solution(input, blinks):
-    stones = input.split(" ")
-    for i in range(blinks):
-        print(f"Blink {i+1}:")
+                else:
+                    updated_stones.append(rule_3(stone))
+        return updated_stones
+
+    stones = [int(x) for x in input.split(" ")]
+    for _ in range(25):
         stones = blink(stones)
     return len(stones)
 
-print(f"Day 11 Test 1: {solution(test_data, 25)}")
-print(f"Day 11 Test 1: {solution(input_str, 25)}")
+def solution_2(input):
+    temp = [int(x) for x in input.split(" ")]
+    stones = defaultdict(int)
+    for stone in temp:
+        stones[stone] += 1
+    def blink(stones):
+        updated_stones = defaultdict(int)
+        for stone, count in stones.items():
+            updated_stone = rule_1(stone)
+            if updated_stone != stone:
+                updated_stones[updated_stone] += count
+            else:
+                updated_stone = rule_2(stone)
+                if updated_stone != stone:
+                    updated_stones[updated_stone[0]] += count
+                    updated_stones[updated_stone[1]] += count
+                else:
+                    updated_stones[rule_3(stone)] += count
+        return updated_stones
+    
+    for _ in range(75):
+        stones = blink(stones)
+    return sum(stones.values())
+
+
+print(f"Day 11 Test 1: {solution_1(test_data)}")
+print(f"Day 11 Test 1: {solution_1(input_str)}")
 
 # Part 2
 
-print(f"Day 11 Test 2: {solution(input_str, 75)}")
+print(f"Day 11 Test 2: {solution_2(input_str)}")
